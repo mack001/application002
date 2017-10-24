3412cf26b17d4f78bbee80ad26eb40779ced0d04
mack he <mack_heyong@yahoo.com>
2017-10-24 05:40:42 -0600

    add comments

---
+++ git test 002/git test 002/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(git_test_002.Startup))]
+namespace git_test_002
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
